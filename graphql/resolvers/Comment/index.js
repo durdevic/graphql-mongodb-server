f20612cc63bf9046e3a1bf965372fdc121143344
Uros <uros@uros.com>
2019-03-08 16:55:29 +0100

    WIP

--- graphql/resolvers/Comment/index.js
+++ graphql/resolvers/Comment/index.js
@@ -2,6 +2,8 @@ import User from "../../../server/models/User";
 import Post from "../../../server/models/Post";
 import Comment from "../../../server/models/Comment";
 
+import {transformComment} from "../merge";
+
 export default {
   Query: {
     comment: async (parent, { _id }, context, info) => {
@@ -28,11 +30,38 @@ export default {
         post: comment.post
       });
 
-      return new Promise((resolve, reject) => {
-        newComment.save((err, res) => {
-          err ? reject(err) : resolve(res);
-        });
-      });
+      let createdComment;
+      try {
+        console.log(comment);
+        const result = await newComment.save();
+        console.log(result)
+        createdComment = transformComment(result);
+        console.log('did you even go ')
+        const post = await Post.findById(comment.post);
+        const author = await User.findById(comment.author);
+
+        if (!post) {
+          throw new Error("Post not found.");
+        } else if (!author) {
+          throw new Error("Author does not exist");
+        }
+        // pushing to Post and Author comments array
+        post.comments.push(newComment);
+        author.comments.push(newComment);
+        await post.save();
+        await author.save();
+        return createdComment;
+
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+
+      // return new Promise((resolve, reject) => {
+      //   newComment.save((err, res) => {
+      //     err ? reject(err) : resolve(res);
+      //   });
+      // });
     },
     updateComment: async (parent, { _id, comment }, context, info) => {
       return new Promise((resolve, reject) => {
--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,24 +1,77 @@
 import User from "./User";
+import Post from "./Post";
+import Comment from "./Comment";
 
 const user = async userId => {
-    try {
-        const user = await User.findById(userId);
-        return {
-            ...user._doc,
-            _id: user.id,
-            createdPosts: postMessage.bind(this, user._doc.createdPosts)
-        };
-    } catch (error) {
-        throw error;
-    }
-}
-
-const transformPost = event => {
+  try {
+    const user = await User.findById(userId);
+    console.log('this is user')
+    console.log(user)
     return {
-        ...event._doc,
-        _id: event.id,
-        creator: user.bind(this, event.creator)
-    }
-}
+      ...user._doc,
+      _id: user.id,
+      posts: posts.bind(this, user._doc.posts),
+      comments: comments.bind(this, user._doc.comments)
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+const post = async postId => {
+  try {
+    const post = await Post.findById(postId);
+    return {
+      ...post._doc,
+      _id: post.id,
+      comments: comments.bind(this, post._doc.comments)
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+const comments = async commentIds => {
+  try {
+    const comments = await Comment.find({ _id: { $in: commentIds } });
+    return comments.map(comment => {
+      return transformComment(comment);
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const posts = async postIds => {
+  try {
+    const posts = await Post.find({ _id: { $in: postIds } });
+    return posts.map(post => {
+      return transformPost(post);
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const transformComment = comment => {
+  return {
+    ...comment._doc,
+    _id: comment.id,
+    author: user.bind(this, comment._doc.author),
+    post: post.bind(this, comment._doc.post)
+  };
+};
+
+const transformPost = post => {
+  console.log('transforming postj')
+  console.log(post)
+  return {
+    ...post._doc,
+    _id: post.id,
+    author: user.bind(this, post._doc.author),
+    comments: comments.bind(this, post._doc.comments)
+  };
+};
 
 exports.transformPost = transformPost;
+exports.transformComment = transformComment;
